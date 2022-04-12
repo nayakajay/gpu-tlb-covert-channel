9c09b89a33544977da171e12dc739c31750487e8
Ajay Nayak <nayakajay176@gmail.com>
2022-04-12 12:32:34 +0530

    Cleaning-up content from support.h

--- support.h
+++ support.h
@@ -8,26 +8,10 @@ typedef struct {
     struct timeval endTime;
 } Timer;
 
-#ifdef __cplusplus
-extern "C" {
-#endif
 void verify(float *A, float *B, float *C, unsigned int m, unsigned int k,
   unsigned int n);
 void startTime(Timer* timer);
 void stopTime(Timer* timer);
 float elapsedTime(Timer timer);
-#ifdef __cplusplus
-}
-#endif
-
-#define FATAL(msg, ...) \
-    do {\
-        fprintf(stderr, "[%s:%d] "msg"\n", __FILE__, __LINE__, ##__VA_ARGS__);\
-        exit(-1);\
-    } while(0)
-
-#if __BYTE_ORDER != __LITTLE_ENDIAN
-# error "File I/O is not implemented for this system: wrong endianness."
-#endif
 
 #endif
